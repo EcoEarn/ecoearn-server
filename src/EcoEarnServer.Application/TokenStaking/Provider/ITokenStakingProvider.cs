f7bff28232acfafeb44921b81e4bd4a1152f7fa1
Quinn <gavin0428@ecoearn.io>
2024-05-23 21:22:35 +0800

    fix

--- src/EcoEarnServer.Application/TokenStaking/Provider/ITokenStakingProvider.cs
+++ src/EcoEarnServer.Application/TokenStaking/Provider/ITokenStakingProvider.cs
@@ -209,7 +209,7 @@ public class TokenStakingProvider : ITokenStakingProvider, ISingletonDependency
                             startBlockNumber
                             endBlockNumber
                             rewardPerBlock
-                            UnlockWindowDuration
+                            unlockWindowDuration
                             stakingToken
                             fixedBoostFactor
                             minimumAmount
