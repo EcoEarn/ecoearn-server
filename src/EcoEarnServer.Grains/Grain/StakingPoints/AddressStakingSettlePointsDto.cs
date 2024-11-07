d0ef731fb0d14b21a022e560fb211acb6a78ea2a
Quinn <gavin0428@ecoearn.io>
2024-11-07 13:26:03 +0800

    feat: upgrade

--- src/EcoEarnServer.Grains/Grain/StakingPoints/AddressStakingSettlePointsDto.cs
+++ src/EcoEarnServer.Grains/Grain/StakingPoints/AddressStakingSettlePointsDto.cs
@@ -2,12 +2,19 @@ using EcoEarnServer.StakingSettlePoints;
 
 namespace EcoEarnServer.Grains.Grain.StakingPoints;
 
+[GenerateSerializer]
 public class AddressStakingSettlePointsDto
 {
+    [Id(0)]
     public string Id { get; set; }
+    [Id(1)]
     public string Address { get; set; }
+    [Id(2)]
     public string Points { get; set; }
+    [Id(3)]
     public List<StakingSettlePointsDto> DappPoints { get; set; }
+    [Id(4)]
     public long CreateTime { get; set; }
+    [Id(5)]
     public long UpdateTime { get; set; }
 }
